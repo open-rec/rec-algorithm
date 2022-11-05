0a3068c3b26db2eae09010c1884c50e835f2b13c
xsank <xsank@foxmail.com>
2022-11-05 17:37:55 +0800

    add package support

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='rec-algorithm',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    include_package_data=False,
+    author='xsank',
+)
