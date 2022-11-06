afd4960a3448c0c57e800f6d3b207b367fe55766
xsank <xsank@foxmail.com>
2022-11-06 14:30:05 +0800

    add hot&new gen recall data

--- tool/gen_recall_data.py
+++ tool/gen_recall_data.py
@@ -2,7 +2,9 @@ import csv
 
 import pandas as pd
 
+from algorithm.recall.hot import Hot
 from algorithm.recall.i2i import ItemBasedI2I
+from algorithm.recall.new import New
 
 
 def gen_i2i_data(items, events, i2i_size, filename):
@@ -23,11 +25,25 @@ def gen_embedding_data(items, events, embedding_size, filename):
 
 
 def gen_hot_data(events, hot_size, filename):
-    pass
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['scene', 'item', 'score'])
+        for scene, scene_events in events.groupby('scene'):
+            scene_hot = Hot(events=scene_events, recall_size=hot_size)
+            recall_items = scene_hot.recall()
+            for score_item in recall_items:
+                writer.writerow([scene, score_item.item, score_item.score])
 
 
 def gen_new_data(items, new_size, filename):
-    pass
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['scene', 'item', 'score'])
+        for scene, scene_items in items.groupby('scene'):
+            scene_new = New(items=scene_items, recall_size=new_size)
+            recall_items = scene_new.recall()
+            for score_item in recall_items:
+                writer.writerow([scene, score_item.item, score_item.score])
 
 
 if __name__ == "__main__":
@@ -37,5 +53,5 @@ if __name__ == "__main__":
 
     gen_i2i_data(items, events, 50, '../data/i2i.csv')
     gen_embedding_data(items, events, 20, '../data/embedding.csv')
-    gen_hot_data(events, 200, '../data/hot.csv')
-    gen_new_data(items, 100, '../data/new.csv')
+    gen_hot_data(events, 2000, '../data/hot.csv')
+    gen_new_data(items, 2000, '../data/new.csv')
