18a99ba4acacd61243647897d2570a878e605f93
xsank <xsank@foxmail.com>
2022-11-10 16:21:53 +0800

    format code

--- tool/gen_recall_data.py
+++ tool/gen_recall_data.py
@@ -2,9 +2,9 @@ import csv
 
 import pandas as pd
 
+from algorithm.recall.embedding import EventEmbedding
 from algorithm.recall.hot import Hot
 from algorithm.recall.i2i import ItemBasedI2I
-from algorithm.recall.embedding import EventEmbedding
 from algorithm.recall.new import New
 
 
@@ -28,8 +28,9 @@ def gen_embedding_data(items, events, embedding_dim, filename):
         for scene, scene_events in events.groupby('scene'):
             scene_embedding = EventEmbedding(events=scene_events)
             item_vectors = scene_embedding.dump_vectors(embedding_dim)
-            for item,vector in item_vectors:
+            for item, vector in item_vectors:
                 writer.writerow([scene, item, vector])
+
 
 def gen_hot_data(events, hot_size, filename):
     with open(filename, 'w') as f:
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     items = pd.read_csv('../data/item.csv', header=0)
     events = pd.read_csv('../data/event.csv', header=0)
 
-    #gen_i2i_data(items, events, 50, '../data/recall/i2i.csv')
+    gen_i2i_data(items, events, 50, '../data/recall/i2i.csv')
     gen_embedding_data(items, events, 10, '../data/embedding.csv')
-    #gen_hot_data(events, 2000, '../data/recall/hot.csv')
-    #gen_new_data(items, 2000, '../data/recall/new.csv')
+    gen_hot_data(events, 2000, '../data/recall/hot.csv')
+    gen_new_data(items, 2000, '../data/recall/new.csv')
