0d1f0afc420ea2fa79f3767ff6b60ad83f3dc2a5
xsank <xsank@foxmail.com>
2024-07-28 15:28:24 +0800

    modify package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name='rec-algorithm',
     version='0.0.1',
-    packages=setuptools.find_packages(),
-    include_package_data=False,
+    packages=find_packages(),
+    include_package_data=True,
     author='xsank',
 )
